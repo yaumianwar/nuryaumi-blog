d1da6b2a3cc7828f9d3180b2313570d24f826a08
yaumianwar <nuryaumi10@gmail.com>
2023-06-13 19:10:06 +0800

    change character &apos;

--- pages/about.tsx
+++ pages/about.tsx
@@ -34,7 +34,7 @@ export default function Page() {
         <div className="mt-2 flex flex-col space-y-4 text-slate-500">
           <p>
             Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
-            Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
+            Lorem Ipsum has been the industry&apos;s standard dummy text ever since the 1500s, 
             when an unknown printer took a galley of type and scrambled it to make a type specimen book. 
             It has survived not only five centuries, but also the leap into electronic typesetting, 
             remaining essentially unchanged.
@@ -42,7 +42,7 @@ export default function Page() {
 
           <p>
             Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
-            Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
+            Lorem Ipsum has been the industry&apos;s standard dummy text ever since the 1500s, 
             when an unknown printer took a galley of type and scrambled it to make a type specimen book. 
             It has survived not only five centuries, but also the leap into electronic typesetting, 
             remaining essentially unchanged.
@@ -50,7 +50,7 @@ export default function Page() {
 
           <p>
             Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
-            Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
+            Lorem Ipsum has been the industry&apos;s standard dummy text ever since the 1500s, 
             when an unknown printer took a galley of type and scrambled it to make a type specimen book. 
             It has survived not only five centuries, but also the leap into electronic typesetting, 
             remaining essentially unchanged.
